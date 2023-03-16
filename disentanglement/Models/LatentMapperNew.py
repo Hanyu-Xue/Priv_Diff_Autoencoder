7d46b494a037a25851641a0e0b35beb5e0c208ae
Joshua Xue <47020132+Hanyu-Xue@users.noreply.github.com>
2023-03-16 14:52:41 +1100

    Add files via upload

--- disentanglement/Models/LatentMapperNew.py
+++ disentanglement/Models/LatentMapperNew.py
@@ -1,32 +1,32 @@
 import torch.nn as nn
 
 class LatentMapper(nn.Module):
-    def __init__(self, dropout_rate=0.2):
+    def __init__(self, dropout_rate=0.2, batch_size = 2):
         super().__init__()
         slope = 0.2
         self.model = nn.Sequential(
             nn.Linear(2560, 2048),
-            nn.InstanceNorm1d(2048),
+            nn.BatchNorm1d(2048, track_running_stats=False if batch_size == 1 else True),
             nn.LeakyReLU(negative_slope=slope),
             nn.Dropout(dropout_rate),
             
             nn.Linear(2048, 2048),
-            nn.InstanceNorm1d(2048),
+            nn.BatchNorm1d(2048, track_running_stats=False if batch_size == 1 else True),
             nn.LeakyReLU(negative_slope=slope),
             nn.Dropout(dropout_rate),
             
             nn.Linear(2048,1024),
-            nn.InstanceNorm1d(1024),
+            nn.BatchNorm1d(1024, track_running_stats=False if batch_size == 1 else True),
             nn.LeakyReLU(negative_slope=slope),
             nn.Dropout(dropout_rate),
             
             nn.Linear(1024,1024),
-            nn.InstanceNorm1d(1024),
+            nn.BatchNorm1d(1024, track_running_stats=False if batch_size == 1 else True),
             nn.LeakyReLU(negative_slope=slope),
             nn.Dropout(dropout_rate),
             
             nn.Linear(1024, 512),
-            nn.InstanceNorm1d(512),
+            nn.BatchNorm1d(512, track_running_stats=False if batch_size == 1 else True),
             nn.LeakyReLU(negative_slope=slope),
             nn.Dropout(dropout_rate),
             
@@ -34,7 +34,7 @@ class LatentMapper(nn.Module):
         )
         for m in self.model:
             if isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight, a= slope)
+                nn.init.kaiming_normal_(m.weight, a=slope)
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, input_tensor):
